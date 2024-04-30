6f8bae4dd3186508727eb58c3116be7e11fcb854
raja8287 <71815222+raja8287@users.noreply.github.com>
2024-04-30 14:25:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express=require("express");
 const app=express();
-
 require("dotenv").config();
 var bodyParser = require("body-parser");
 const Port=process.env.Port ||4000
-const ud=require("./dbconfig/dbConfig")
 const cors=require('cors');
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
