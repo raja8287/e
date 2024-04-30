5f30dbc1abee565d42f1cc969a5e296140a27565
raja8287 <71815222+raja8287@users.noreply.github.com>
2024-04-30 14:26:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const Port=process.env.Port ||4000
 const cors=require('cors');
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
-app.use(express.json({limit:'500mb'}));
-app.use(express.urlencoded({limit:'500mb'}));
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({limit:'50mb'}));
 app.use(bodyParser.json());
 
 
